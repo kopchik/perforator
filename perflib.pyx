374cfa6f4e000cea9143f4a5c1d87cbcab2b0072
Alexandre Kandalintsev <spam@messir.net>
2014-04-11 16:10:43 +0200

    .

--- perflib.pyx
+++ perflib.pyx
@@ -2,8 +2,12 @@ from cpython.mem cimport PyMem_Malloc, PyMem_Free
 from libc.stdint cimport int64_t, uint32_t, uint64_t
 from posix.unistd cimport pid_t, useconds_t, read, usleep
 from posix.ioctl cimport ioctl
+from libc.signal cimport SIGCONT, SIGSTOP
 from cython cimport sizeof
 import os
+
+cdef extern from "signal.h" nogil:
+  int kill(pid_t pid, int sig) except -1
 
 cdef extern from "linux/perf_event.h":
   cdef struct perf_event_attr:
@@ -11,49 +15,74 @@ cdef extern from "linux/perf_event.h":
     uint32_t size
     uint64_t config
     uint64_t disabled
+    uint64_t read_format
+
   cdef enum perf_type_id:
     PERF_TYPE_HARDWARE
     PERF_COUNT_HW_INSTRUCTIONS
+    PERF_COUNT_HW_CPU_CYCLES
   cdef int PERF_EVENT_IOC_RESET
+  cdef int PERF_FORMAT_GROUP
+  cdef int PERF_IOC_FLAG_GROUP
+  cdef int PERF_FORMAT_TOTAL_TIME_ENABLED
+  cdef int PERF_FORMAT_TOTAL_TIME_RUNNING
 
 cdef extern from "_perf.h":
   void* mymalloc(size_t size)
   int perf_event_open(perf_event_attr *event, pid_t pid,
-                  int cpu, int group_fd, unsigned long flags)
+                  int cpu, int group_fd, unsigned long flags) except -1
 
-  
+
+cdef struct Result:
+  uint64_t nr
+  uint64_t time_enabled
+  uint64_t time_running
+  uint64_t instr
+  uint64_t cycles
+
+
 cdef class Task:
   cdef pid_t pid
-  cdef int fd
+  cdef int ifd, cfd
 
   def __cinit__(self, pid_t pid=0):
-    cdef int fd, pe_size
+    cdef int pe_size
     cdef perf_event_attr *pe
 
-    os.kill(pid, 0)
+    kill(pid, 0)
     self.pid = pid
 
     pe_size = sizeof(perf_event_attr)
     pe = <perf_event_attr*>mymalloc(pe_size)
+
     pe.size = pe_size
     pe.type = PERF_TYPE_HARDWARE
     pe.disabled = 0
+    pe.read_format = PERF_FORMAT_GROUP | PERF_FORMAT_TOTAL_TIME_ENABLED | PERF_FORMAT_TOTAL_TIME_RUNNING
     pe.config = PERF_COUNT_HW_INSTRUCTIONS
-    fd = perf_event_open(pe, pid, -1, -1, 0)
-    assert fd, "failed to open fd"
-    self.fd = fd
+    self.ifd = perf_event_open(pe, pid, -1, -1, 0)
 
-  def measure(self, double interval):
+    pe.config = PERF_COUNT_HW_CPU_CYCLES
+    self.cfd = perf_event_open(pe, pid, -1, self.ifd, 0)
+
+  cpdef measure(self, double interval):
     cdef int r
-    cdef uint64_t cnt
-    r = ioctl(self.fd, PERF_EVENT_IOC_RESET, 0)
+    cdef Result cnts
+
+    r = ioctl(self.ifd, PERF_EVENT_IOC_RESET, PERF_IOC_FLAG_GROUP)
     assert r != -1, "ioctl PERF_EVENT_IOC_RESET failed"
-    usleep(<useconds_t>interval*(10**6))  # convert seconds to microseconds
-    r = read(self.fd, &cnt, sizeof(cnt))
-    assert r != -1
-    return cnt
-  def measurex(self, double interval, int num):
-    cdef uint64_t r[num]
-    for x in range(num):
-      r[num] = x
-    return r
+    usleep(<useconds_t>(interval*(10.0**6)))  # convert seconds to microseconds
+    r = read(self.ifd, &cnts, sizeof(cnts))
+    assert r == sizeof(cnts)
+    print("!", cnts.time_running, cnts.time_enabled)
+    assert cnts.time_running == cnts.time_enabled
+    return (cnts.instr, cnts.cycles)
+
+  cpdef measurex(self, double interval, int num):
+    return [self.measure(interval) for _ in range(num)]
+
+  cpdef stop(self):
+    kill(self.pid, SIGSTOP)
+
+  cpdef cont(self):
+    kill(self.pid, SIGCONT)
--- perforator.py
+++ perforator.py
@@ -1,6 +1,36 @@
 #!/usr/bin/env python3
-
+import pyximport; pyximport.install(pyimport = True)
 from perflib import Task
 
-t = Task(0)
-print(t.measurex(0.01,1))
+from time import time, sleep
+from psutil import process_iter
+import math
+
+PERIOD = 0.01
+TIME = 0.1
+TIMES = round(TIME/PERIOD)
+THRESH = 10 # min CPU usage in %
+
+
+def get_heavy_tasks(thr=THRESH, t=0.3):
+	[p.cpu_percent() for p in process_iter()]
+	sleep(t)
+	## short version
+	# return [p.pid for p in process_iter() if p.cpu_percent()>10]
+	r = []
+	for p in process_iter():
+		cpu = p.cpu_percent()
+		if cpu > 10:
+			print("{pid:<7} {name:<12} {cpu}".format(pid=p.pid, name=p.name(), cpu=cpu))
+			r.append(p.pid)
+	return r
+
+
+if __name__ == '__main__':
+	pids = get_heavy_tasks()
+	tasks = [Task(p) for p in pids]
+
+	# for p in pids:
+	t = Task(0)
+	for x in range(4):
+		print(t.measure(PERIOD))
