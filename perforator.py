d3ec488bcb5dbdd1fda360905616a0d4eb6dd3ac
Alexandre Kandalintsev <spam@messir.net>
2014-04-14 13:53:50 +0200

    perforator.py: added argument parser

--- perforator.py
+++ perforator.py
@@ -6,11 +6,9 @@ from psutil import process_iter
 from subprocess import Popen
 from time import time, sleep
 from statistics import mean
+import argparse
 import atexit
 
-PERIOD = 0.01
-TIME = 0.1
-TIMES = round(TIME/PERIOD)
 THRESH = 10 # min CPU usage in %
 
 
@@ -36,15 +34,15 @@ def unpack(tuples):
   return r1,r2
 
 
-def profile(tasks):
+def profile(tasks, interval, samples):
   assert len(tasks) > 1, "at least two tasks should be given"
   while True:
     for t in tasks:
       # phase 1: measure with other tools
-      shared, _ = unpack(t.measurex(PERIOD, TIMES))
+      shared, _ = unpack(t.measurex(interval/samples, samples))
       t.freeze(tasks)
       # phase 2: exclusive resource usage
-      exclusive, _ = unpack(t.measurex(PERIOD, TIMES))
+      exclusive, _ = unpack(t.measurex(interval/samples, samples))
       t.defrost(tasks)
       print(mean(shared)/mean(exclusive))
       sleep(1)
@@ -57,7 +55,15 @@ def prepare():
 
 
 if __name__ == '__main__':
+  parser = argparse.ArgumentParser(description='Run experiments')
+  parser.add_argument('-i', '--interval', type=float, default=0.1, help="measurement time (in _seconds_!)")
+  parser.add_argument('-n', '--num-samples', type=int, default=10, help="samples per second")
+  parser.add_argument('-d', '--debug', default=False, const=True, action='store_const', help='enable debug mode')
+  args = parser.parse_args()
+
+
+
   prepare()
   pids = get_heavy_tasks()
   tasks = [Task(p) for p in pids]
-  profile(tasks)
+  profile(tasks, args.interval, args.num_samples)
