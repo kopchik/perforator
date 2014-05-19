220d0f46a18d358a719474adc140d842b53be255
Alexandre Kandalintsev <spam@messir.net>
2014-05-19 13:15:43 +0000

    new file:   results/reverse_num=1,pause=20,time=360_benches=sdag_sdagp_blosc_wordpress.pickle

--- exp.sh
+++ exp.sh
@@ -2,6 +2,10 @@
 
 #time  ./perforator.py -t 'func=reverse|num=1|time=360|pause=20' -o auto
 #time  ./perforator.py -t 'func=reverse|num=10000|time=0.1|pause=0.1' -o auto
-time ./perforator.py -t 'func=reverse|num=1000|time=0.1|pause=0.1' \
-  -b sdag sdagp blosc wordpress -o "reverse_num=1000,pause=0.1,time=0.1_benches=sdag_sdagp_blosc_wordpress.pickle"
+
+time ./perforator.py -t 'func=reverse|num=1|time=360|pause=20' \
+  -b sdag sdagp blosc wordpress -o "reverse_num=1,pause=20,time=360_benches=sdag_sdagp_blosc_wordpress.pickle"
+#time ./perforator.py -t 'func=reverse|num=1000|time=0.1|pause=0.1' \
+#  -b sdag sdagp blosc wordpress -o "reverse_num=1000,pause=0.1,time=0.1_benches=sdag_sdagp_blosc_wordpress.pickle"
+
 
