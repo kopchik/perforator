063da6b35b80a9d114299e47d6054eba48ea0fc0
Alexandre Kandalintsev <spam@messir.net>
2014-06-02 12:31:01 +0000

    modified:   exp.sh

--- exp.sh
+++ exp.sh
@@ -17,4 +17,6 @@
 #time ./perforator.py -t 'func=distribution|num=1000|interval=0.05' -o auto
 #time ./perforator.py -t 'func=distribution|num=1000|interval=0.02' -o auto
 
-time ./perforator.py -t 'func=reverse_interference|num=1000|time=0.1|real_time=360' -o auto
+#time ./perforator.py -t 'func=reverse|num=100|time=0.1' -o auto
+#time ./perforator.py -t 'func=reverse|num=1000|time=0.2' -o auto
+#time ./perforator.py -t 'func=reverse|num=1000|time=0.3' -o auto
