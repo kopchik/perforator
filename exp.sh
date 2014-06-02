0355720e4dd95b1d4d49eaaa0fef32b5eb02dfd3
Alexandre Kandalintsev <spam@messir.net>
2014-06-02 22:56:27 +0200

    exp.sh: run pdb in case of problems

--- exp.sh
+++ exp.sh
@@ -13,8 +13,9 @@
 #  -b sdag sdagp blosc wordpress -o "results/reverse_num=10000,pause=0.1,time=0.1_benches=sdag_sdagp_blosc_wordpress.pickle"
 
 
-#time ./perforator.py -t 'func=distribution|num=1000|interval=0.1' -o auto
+time python -m pdb ./perforator.py -t 'func=distribution|num=1000|interval=0.1' -o auto
 #time ./perforator.py -t 'func=distribution|num=1000|interval=0.05' -o auto
-#time ./perforator.py -t 'func=distribution|num=1000|interval=0.02' -o auto
+time python -m pdb ./perforator.py -t 'func=distribution|num=1000|interval=0.02' -o auto
+time python -m pdb ./perforator.py -t 'func=distribution|num=1000|interval=0.01' -o auto
 
-time ./perforator.py -t 'func=reverse_interference|num=1000|time=0.1|real_time=360' -o auto
+#time ./perforator.py -t 'func=reverse_interference|num=1000|time=0.1|real_time=360' -o auto
