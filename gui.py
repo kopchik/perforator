054cef2e630afd7aaf62bf67048dc37414893db5
Alexandre Kandalintsev <exe.sre@gmail.com>
2015-01-27 22:45:50 +0000

    Full rewrite of gui

--- gui.py
+++ gui.py
@@ -1,53 +1,19 @@
 #!/usr/bin/env python3
+
 from perf.qemu import NotCountedError
-from curses import wrapper, newwin
 from threading import Thread
 from collections import defaultdict, deque
-from queue import Queue
-from curses import *
+# from queue import Queue
 import time
-
-"""
-main screen
-cmd on the bottom with history
-"""
-
+import sys
+from libgui import Border, Bars, Button, Canvas, XY, Text, CMDInput, VList, mywrapper
+import curses
+from config import VMS
 
 class Stat:
   def __init__(self):
     self.shared = deque(maxlen=10)
     self.isolated = deque(maxlen=10)
-
-
-def mywrapper(f):
-  return lambda *args, **kwargs: wrapper(f, *args, **kwargs)
-
-
-class Win:
-  def __init__(self, cwin, decor=True):
-    self.cwin = cwin
-    self.decor = decor
-  def clear(self, color=None):
-    self.cwin.clear()
-    if self.decor:
-      self.cwin.border()
-    self.cwin.refresh()
-
-
-class TopWin(Win):
-  def __init__(self, *args, vms=[], **kwargs):
-    super().__init__(*args, **kwargs)
-    self.vms = vms
-    self.vmstat = defaultdict(Stat)
-
-    self.collector = Thread(target=collect, args=(self.vms, self.vmstat), kwargs={'cb':self.show})
-    self.collector.daemon = True
-    self.collector.start()
-
-  def show(self):
-    for i, vm in enumerate(self.vms):
-      stat = self.vmstat[vm]
-      self.cwin.addstr(i, 0, "XXX")
 
 
 def collect(vms, vmstat, measure_time=0.1, interval=0.1, cb=None):
@@ -72,15 +38,67 @@ def collect(vms, vmstat, measure_time=0.1, interval=0.1, cb=None):
       finally:
         vm.shared()
 
-@mywrapper
-def top(scr, vms=[]):
-  #scr.clear()
-  max_y, max_x = scr.getmaxyx()
-  topcwin = scr.derwin(len(vms)*2+2, max_x, 0, 0)
-  top = TopWin(topcwin, vms=vms)
-  #print(dir(scr))
-  #print(vms)
-  time.sleep(3)
 
-from config import VMS as vms
-top(vms=vms)
+@mywrapper
+def gui(scr):
+  # setup canvas
+  size_y, size_x = scr.getmaxyx()
+  size = XY(size_x, size_y)
+  canvas = Canvas(scr, size)
+  canvas.clear()
+
+  # widget hierarchy
+  root = \
+      Border(
+          VList(
+              Border(
+                  Bars([0.01, 0.5, 0.7, 1])),
+              Border(
+                  Text(id='logwin'),
+                  label="Logs"),
+              CMDInput(id='cmdinpt'),
+              Button("QUIT", cb=sys.exit)),
+          label="Per-Core Performane")
+
+  # setup callbacks
+  logwin = root['logwin']
+  def cb(s):
+    tstamp = time.strftime("%H:%M:%S", time.localtime())
+    logwin.println("{} {}".format(tstamp, s))
+    if s == 'quit':
+      sys.exit()
+  root['cmdinpt'].cb = cb
+
+  # calculate widget placement and draw widgets
+  root.init(pos=XY(0, 0), maxsize=size, canvas=canvas)
+  root.setup_sigwinch()
+  root.draw()
+
+  # top-like bars
+  vmstat = defaultdict(Stat)
+  collector = Thread(target=collect,
+                      args=(VMS, vmstat),
+                      # kwargs={'cb':self.show}
+                      )
+  collector.daemon = True
+  collector.start()
+
+
+  # main loop
+  if root.cur_focus:
+    root.cur_focus.on_focus()
+  while True:
+    try:
+      key = scr.getkey()
+    except KeyboardInterrupt:
+      break
+    except curses.error:
+      # this is very likely to caused by terminal resize O_o
+      continue
+    if key == '\x1b':
+      break
+    if root.cur_focus:
+      root.cur_focus.input(key)
+
+if __name__ == '__main__':
+  gui()
