d3f7ade50f3d00faa4afc800023d55c62b035063
Alexandre Kandalintsev <spam@messir.net>
2015-03-20 13:40:12 +0100

    exp.sh: removed debug stuff

--- exp.sh
+++ exp.sh
@@ -13,10 +13,10 @@
 #  -b sdag sdagp blosc wordpress -o "results/reverse_num=10000,pause=0.1,time=0.1_benches=sdag_sdagp_blosc_wordpress.pickle"
 
 
-time python -m pdb ./perforator.py -t 'func=distribution|num=1000|interval=0.1' -o auto
-time python -m pdb ./perforator.py -t 'func=distribution|num=1000|interval=0.05' -o auto
-time python -m pdb ./perforator.py -t 'func=distribution|num=1000|interval=0.02' -o auto
-time python -m pdb ./perforator.py -t 'func=distribution|num=1000|interval=0.01' -o auto
+./perforator.py -t 'func=distribution|num=1000|interval=0.1' -o auto
+./perforator.py -t 'func=distribution|num=1000|interval=0.05' -o auto
+./perforator.py -t 'func=distribution|num=1000|interval=0.02' -o auto
+./perforator.py -t 'func=distribution|num=1000|interval=0.01' -o auto
 
 #time ./perforator.py -t 'func=reverse_interference|num=1000|time=0.1|real_time=360' -o auto
 #time ./perforator.py -t 'func=reverse|num=100|time=0.1' -o auto
