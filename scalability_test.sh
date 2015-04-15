a649f978dd1a0231e7d52115d279981a6ab2fc3c
Alexandre Kandalintsev <spam@messir.net>
2015-04-15 11:06:10 +0000

    scalability_test.sh: more tests

--- scalability_test.sh
+++ scalability_test.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
-CMD="/home/sources/perftest/benches/test_SDAG/test_sdag+ -t 5 -q 1000 /home/sources/perftest/benches/test_SDAG/dataset.dat"
+#CMD="/home/sources/perftest/benches/test_SDAG/test_sdag+ -t 5 -q 1000 /home/sources/perftest/benches/test_SDAG/dataset.dat"
 #CMD="/home/sources/perftest/benches/test_SDAG/test_sdag -t 5 -q 1000 /home/sources/perftest/benches/test_SDAG/dataset.dat"
+#CMD="bencher.py -s 100000 -- ffmpeg -i /home/sources/avatar_trailer.m2ts \
+#-strict -2 \
+#-threads 1 -t 10 -y -strict -2 -loglevel panic \
+#-acodec aac -aq 100 \
+#-vcodec libx264 -preset fast -crf 22 \
+#-f mp4 /dev/null"
+CMD="/home/sources/perftest/benches/matrix.py -s 512 -r 100000"
+
 OUTPUT="./scalability_results.txt"
 TIME=60
 rm $OUTPUT
