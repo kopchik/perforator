48f4c95e4b0d6c849796888e039b86b063aa7f81
Alexandre Kandalintsev <spam@messir.net>
2015-04-28 11:52:09 +0000

    modified:   exp.sh

--- exp.sh
+++ exp.sh
@@ -13,10 +13,10 @@
 #  -b sdag sdagp blosc wordpress -o "results/reverse_num=10000,pause=0.1,time=0.1_benches=sdag_sdagp_blosc_wordpress.pickle"
 
 
-./perforator.py -t 'func=distribution|num=1000|interval=0.1' -o auto
-./perforator.py -t 'func=distribution|num=1000|interval=0.05' -o auto
-./perforator.py -t 'func=distribution|num=1000|interval=0.02' -o auto
-./perforator.py -t 'func=distribution|num=1000|interval=0.01' -o auto
+#./perforator.py -t 'func=distribution|num=1000|interval=0.1' -o auto
+#./perforator.py -t 'func=distribution|num=1000|interval=0.05' -o auto
+#./perforator.py -t 'func=distribution|num=1000|interval=0.02' -o auto
+#./perforator.py -t 'func=distribution|num=1000|interval=0.01' -o auto
 
 #time ./perforator.py -t 'func=reverse_interference|num=1000|time=0.1|real_time=360' -o auto
 #time ./perforator.py -t 'func=reverse|num=100|time=0.1' -o auto
@@ -47,14 +47,34 @@
 #./perforator.py -t "func=distr_subsampling|num=300|interval=0.1|rate=500|skip=1" -o auto; sleep 60
 #./perforator.py -t "func=distr_subsampling|num=300|interval=0.1|rate=500|skip=2" -o auto; sleep 60
 #./perforator.py -t "func=distr_subsampling|num=300|interval=0.1|rate=500|skip=3" -o auto; sleep 60
-./perforator.py -t "func=distr_subsampling|num=1000|interval=0.05|rate=500|skip=1" -o auto
-sleep 60
-./perforator.py -t "func=distr_subsampling|num=1000|interval=0.05|rate=500|skip=2" -o auto
-sleep 60
-./perforator.py -t "func=distr_subsampling|num=1000|interval=0.05|rate=500|skip=3" -o auto
-sleep 60
-./perforator.py -t "func=distr_subsampling|num=1000|interval=0.02|rate=500|skip=1" -o auto
-sleep 60
-./perforator.py -t "func=distr_subsampling|num=1000|interval=0.02|rate=500|skip=2" -o auto
-sleep 60
-./perforator.py -t "func=distr_subsampling|num=1000|interval=0.02|rate=500|skip=3" -o auto
+
+#./perforator.py -t "func=distr_subsampling|num=1000|interval=0.05|rate=500|skip=1" -o auto
+#sleep 60
+#./perforator.py -t "func=distr_subsampling|num=1000|interval=0.05|rate=500|skip=2" -o auto
+#sleep 60
+#./perforator.py -t "func=distr_subsampling|num=1000|interval=0.05|rate=500|skip=3" -o auto
+#sleep 60
+#./perforator.py -t "func=distr_subsampling|num=1000|interval=0.02|rate=500|skip=1" -o auto
+#sleep 60
+#./perforator.py -t "func=distr_subsampling|num=1000|interval=0.02|rate=500|skip=2" -o auto
+#sleep 60
+
+NUM=100
+
+./perforator.py -t "func=distribution,num=$NUM,interval=10"  -o auto
+sleep 20
+./perforator.py -t "func=distribution,num=$NUM,interval=20"  -o auto
+sleep 20
+./perforator.py -t "func=distribution,num=$NUM,interval=50"  -o auto
+sleep 20
+./perforator.py -t "func=distribution,num=$NUM,interval=100"  -o auto
+sleep 20
+./perforator.py -t "func=distribution,num=$NUM,interval=200"  -o auto
+
+time ./perforator.py -t "func=distribution_with_subsampling,num=$NUM,interval=100,subinterval=1"  -o auto
+sleep 20
+time ./perforator.py -t "func=distribution_with_subsampling,num=$NUM,interval=100,subinterval=2"  -o auto
+sleep 20
+time ./perforator.py -t "func=distribution_with_subsampling,num=$NUM,interval=100,subinterval=5"  -o auto
+sleep 20
+time ./perforator.py -t "func=distribution_with_subsampling,num=$NUM,interval=100,subinterval=10"  -o auto
