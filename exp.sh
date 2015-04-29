0cb53ddcc8f7a2d0c032bc67461c58fac592bffd
Alexandre Kandalintsev <spam@messir.net>
2015-04-29 10:54:16 +0000

    exp.sh: fixed typo

--- exp.sh
+++ exp.sh
@@ -9,6 +9,7 @@ for INTERVAL in 10 20 50 100 200; do
   time ./perforator.py -t "func=distribution,num=$NUM,interval=$INTERVAL"  -o auto
   if [ $? -ne 0 ]; then
     exit(1)
+  fi
 done
 
 for SUBINT in 1 2 5 10; do
@@ -18,4 +19,5 @@ for SUBINT in 1 2 5 10; do
   time ./perforator.py -t "func=distribution_with_subsampling,num=$NUM,interval=100,subinterval=$SUBINT"  -o auto
   if [ $? -ne 0 ]; then
     exit(1)
+  fi
 done
