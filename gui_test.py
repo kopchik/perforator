40e099a68ce0d4a721e1419d0ffc9599e48585c2
Alexandre Kandalintsev <spam@messir.net>
2015-06-04 14:53:47 +0000

    gui_test.py: test bars

--- gui_test.py
+++ gui_test.py
@@ -1,8 +1,41 @@
 #!/usr/bin/env python3
 
 from libgui import *
+import time
+from sys import exit
+
+
+@mywrapper
+def gui():
+  num_cores = 8
+  bars = [Bar() for _ in range(8)]
+  root = \
+      Border(
+        VList(
+          HList(*bars),
+          Border(
+            Text(id='logwin'),
+            label="Logs"),
+          Input(),
+          Button(cb=exit),
+        ),
+
+      )
+
+
+  loop(root)
+
+
+  root.initroot()
+  v = 0
+  inpt = myinput(timeout=0.2)
+  while True:
+    v = (v + 0.1) % 1.5
+    [bar.update(v) and time.sleep(0.7) for bar in bars]
+    key = next(inpt)
+    #root['logwin'].println(key)
+    #time.sleep(0.1)
 
 
 if __name__ == '__main__':
-  loop(loop)
-
+  gui()
