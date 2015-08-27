8ba65b8f34b0d56dbca851e2fafa0dd26d14393c
Alexandre Kandalintsev <spam@messir.net>
2015-08-27 00:39:10 +0000

    yet another useless bits and pieces

--- exp.sh
+++ exp.sh
@@ -2,22 +2,31 @@
 
 NUM=1000
 
-for INTERVAL in 10 20 50 100 200; do
+for INTERVAL in 1 2 5 10; do
   sync
   ./qemu.py killall
   sleep 10
-  time ./perforator.py -t "func=distribution,num=$NUM,interval=$INTERVAL"  -o auto
+  ./perforator.py -t "func=distribution,num=$NUM,interval=$INTERVAL"  -o auto
   if [ $? -ne 0 ]; then
     exit 1
   fi
 done
+exit
 
 for SUBINT in 1 2 5 10; do
   sync
   ./qemu.py killall
   sleep 10
-  time ./perforator.py -t "func=distribution_with_subsampling,num=$NUM,interval=100,subinterval=$SUBINT"  -o auto
+#  ./perforator.py -t "func=distribution_with_subsampling,num=$NUM,interval=100,subinterval=$SUBINT"  -o auto
+  ./perforator.py -t "func=distribution_with_subsampling,num=$NUM,interval=50,subinterval=$SUBINT"   -o auto
   if [ $? -ne 0 ]; then
+    echo "ACHTUNG, test terminated unsuccessfully"
+    exit 1
+  fi
+  sleep 10
+  ./perforator.py -t "func=distribution_with_subsampling,num=$NUM,interval=20,subinterval=$SUBINT"   -o auto
+  if [ $? -ne 0 ]; then
+    echo "ACHTUNG, test terminated unsuccessfully"
     exit 1
   fi
 done
